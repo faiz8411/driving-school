1ae03280ced15c2f2df8faf28b00a3470c5785c5
faiz8411 <faizullah.noa2014@gmail.com>
2023-08-26 11:38:23 +0600

    done cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import mongoose from "mongoose";
-
+import app from "./app";
+import config from "./config";
 async function bootstrap() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log("successful");
+    app.listen(config.port, () => {
+      console.log(`driving listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err, "failed");
+  }
 }
 bootstrap();
+
+// rmWAcGpwvm4iR5eM
+// driving_school
